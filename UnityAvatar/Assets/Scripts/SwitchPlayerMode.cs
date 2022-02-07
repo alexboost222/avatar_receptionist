68f0a9334d8dbaa36cb901ce62bbf4bd2f3fb4bb
alexboost222 <alexboost222@gmail.com>
2022-02-07 21:41:44 +0300

    add: WIP om player mode switch

---
+++ UnityAvatar/Assets/Scripts/SwitchPlayerMode.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class SwitchPlayerMode : MonoBehaviour
+{
+    [SerializeField] private Canvas dialogCanvas;
+    [SerializeField] private CharacterController characterController;
+    public enum PlayerMode
+    {
+        Walk,
+        Dialog
+    }
+
+}
