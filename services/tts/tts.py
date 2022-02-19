4f2aaed9faaad6a6548b33b5fe8ce4c634aa205d
alexboost222 <alexboost222@gmail.com>
2022-02-19 19:34:11 +0300

    upd: refactored tts

--- services/tts/tts.py
+++ services/tts/tts.py
@@ -1,15 +1,17 @@
-from transport import Transport
-import sys
+from enum import Enum
 import json
 import requests
 import jwt
 import time
 import os
 
-ROOT = "services/tts"
-CONFIG_FILE_RELATIVE_PATH = f"{ROOT}/config.pem"
-OAUTH_PRIVATE_KEY_FILE_PATH = f"{ROOT}/private.pem"
-SPEECH_FOLDER_PATH = f"{ROOT}/speech"
+TTS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+
+CONFIG_DIRECTORY = "config"
+CONFIG_FILE_PATH = os.path.join(TTS_FOLDER, CONFIG_DIRECTORY, "config.pem")
+OAUTH_PRIVATE_KEY_FILE_PATH = os.path.join(TTS_FOLDER, CONFIG_DIRECTORY, "private.pem")
+
+SPEECH_DIRECTORY_PATH = os.path.join(TTS_FOLDER, "speech")
 
 SERVICE_ACCOUNT_ID_KEY = "service_account_id"
 OAUTH_KEY_ID_KEY = "key_id"
@@ -19,33 +21,29 @@ IAM_TOKEN_EXPIRES_AT_KEY = "expiresAt"
 IAM_TOKEN_CREATE_URL = "https://iam.api.cloud.yandex.net/iam/v1/tokens"
 SYNTHESIZE_SPEECH_URL = "https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize"
 
-NEXT_DEFAULT = "none"
-
-service_account_id = ""
-oauth_key_id = ""
-
-setup_dirty_flag = False
-
 # TODO switch to json config file
-def setup_config():
-    # TODO rewrite global to smth better
-    global service_account_id
-    global oauth_key_id
+def _get_auth_info():
+    service_account_id = ""
+    oauth_key_id = ""
 
-    with open(CONFIG_FILE_RELATIVE_PATH, "r") as config:
+    with open(CONFIG_FILE_PATH, "r") as config:
         config_lines = config.readlines()
 
-        service_account_id_line = next((l for l in config_lines if l.startswith(SERVICE_ACCOUNT_ID_KEY)), NEXT_DEFAULT)
-        if service_account_id_line != NEXT_DEFAULT:
+        next_default = "none"
+
+        service_account_id_line = next((l for l in config_lines if l.startswith(SERVICE_ACCOUNT_ID_KEY)), next_default)
+        if service_account_id_line != next_default:
             service_account_id = service_account_id_line.strip().split()[1]
 
-        oauth_key_id_line = next((l for l in config_lines if l.startswith(OAUTH_KEY_ID_KEY)), NEXT_DEFAULT)
-        if oauth_key_id_line != NEXT_DEFAULT:
+        oauth_key_id_line = next((l for l in config_lines if l.startswith(OAUTH_KEY_ID_KEY)), next_default)
+        if oauth_key_id_line != next_default:
             oauth_key_id = oauth_key_id_line.strip().split()[1]
+    
+    return (service_account_id, oauth_key_id)
 
-def get_auth_jwt():
+def _get_auth_jwt(service_account_id, oauth_key_id):
     with open(OAUTH_PRIVATE_KEY_FILE_PATH, "r") as private:
-        private_key = private.read() # Reading the private key from the file.
+        private_key = private.read()
 
     now = int(time.time())
     payload = {
@@ -54,7 +52,6 @@ def get_auth_jwt():
             "iat": now,
             "exp": now + 360}
 
-    # JWT generation.
     auth_jwt = jwt.encode(
         payload,
         private_key,
@@ -64,7 +61,7 @@ def get_auth_jwt():
 
     return auth_jwt
 
-def create_iam_token(auth_jwt):
+def _get_iam_token(auth_jwt):
     params = { "jwt": auth_jwt }
     response = requests.post(IAM_TOKEN_CREATE_URL, params=params)
     decoded_response = response.content.decode("UTF-8")
@@ -73,7 +70,7 @@ def create_iam_token(auth_jwt):
     expires_at = response_json.get(IAM_TOKEN_EXPIRES_AT_KEY)
     return (iam_token, expires_at)
 
-def synthesize(text, emotion, iam_token):
+def _synthesize(text, format, iam_token):
     headers = {
         "Authorization": f"Bearer {iam_token}",
     }
@@ -82,9 +79,7 @@ def synthesize(text, emotion, iam_token):
         "text": text,
         "lang": "ru-RU",
         "speed": 1.0,
-        "emotion": emotion,
-        "format": "lpcm",
-        "sampleRateHertz": 48000,
+        "format": format,
         "voice": "ermil:rc",
     }
 
@@ -95,20 +90,15 @@ def synthesize(text, emotion, iam_token):
     return response.content
 
 def handle(input):
-    # TODO use class and methods instead of functions
-    global setup_dirty_flag
-
-    if not setup_dirty_flag:
-        setup_config()
-        setup_dirty_flag = True
-    
-    text = input["msg"]
-    auth_jwt = get_auth_jwt()
+    (service_account_id, oauth_key_id) = _get_auth_info()
+    auth_jwt = _get_auth_jwt(service_account_id, oauth_key_id)
     # TODO use expires at
-    (iam_token, expires_at) = create_iam_token(auth_jwt)
-    synt_result = synthesize(text, "good", iam_token)
+    (iam_token, expires_at) = _get_iam_token(auth_jwt)
 
-    synt_file_path = f"{SPEECH_FOLDER_PATH}/{hash(text)}.raw"
+    text = input["msg"]
+    synt_result = _synthesize(text, "oggopus", iam_token)
+
+    synt_file_path = f"{SPEECH_DIRECTORY_PATH}/{hash(text)}.ogg"
     synt_file_path = os.path.abspath(synt_file_path)
 
     with open(synt_file_path, "wb") as syn_result:
@@ -116,8 +106,5 @@ def handle(input):
 
     return { "avatar_speech_filepath": synt_file_path }
 
-t = Transport(handle, "tts")
-
-t.arg_parse(sys.argv)
-
-t.work()
+print(__file__)
+handle({ "msg": "Привет!"})
